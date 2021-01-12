864aa1b86fd8f398fe7614e4b99813115bf47170
Shei <>
2021-01-12 17:57:40 +0800

    test

---
+++ python.py
@@ -0,0 +1,20 @@
+import json
+# the file to be converted to  
+# json format 
+filename = 'data.txt'
+  
+# dictionary where the lines from 
+# text will be stored 
+
+
+list_of_lists = []
+
+with open(filename) as fh: 
+    counter = 1
+    for line in fh: 
+        list_of_lists.append(line[1:])
+
+
+out_file = open("data.json", "w") 
+json.dump(list_of_lists, out_file) 
+out_file.close() 
